5fc92fd8085f7225b89c5b443cc5f0b602f5c4cd
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-23 11:05:07 -0400

    okay

--- src/VehicleClass.java
+++ src/VehicleClass.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class VehicleClass {
+class Main {
+
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println(myCar.getMake());
+        myCar.setMake("Chevy");
+        System.out.println(myCar.getMake());
+    }
+}
+
+class Vehicle {
+    //POJO:  Plain Old Java Object
+    private String make;
+    private String model;
+
+    public Vehicle(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+   public String getModel() {
+        return model;
+   }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
 }
